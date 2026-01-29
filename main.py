354d9296cf9fb6f03b674c69c265eca72fb860bc
Victoria Di Rito <victoriadirito786@gmail.com>
2026-01-29 12:38:55 +0100

    Initialize main.py with Libro and Utente instances

---
+++ main.py
@@ -0,0 +1,9 @@
+from libri import Libro
+from utente import Utente
+from biblioteca import Biblioteca
+
+if __name__ == "__main__":
+
+    Libro1 = Libro("Il signore degli anelli, 21563568, J. R. R. Tolkien")
+    Utente1 = Utente("Veronica rossi, 7413")
+
